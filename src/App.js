e4f2d43f06cf18401db214ac8abceb613a9e34a3
IlhamMaulana71 <ilhammlna340@gmail.com>
2023-03-18 11:49:50 +0700

    Tugas Redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
+import Redux from "./Tugas/Redux/TugasRedux";
 //import TugasRouting from "./Tugas/Routing/TugasRouting";
 //import LandingFuntionComponents from "./Tugas/Hook/IndexFunctionComponents";
 //import LandingNews from "./Tugas/Lifecycle Components";
 //import Tugas from "../src/Tugas/Basic Components/Tugas.jsx"
 //import TugasStylingComponents from "../src/Tugas/Styling Components/Tugas Styling Components.jsx"
-import TugasFromRegister from "../src/Tugas/Form and Validation/Tugas From Register.jsx"
+//import TugasFromRegister from "../src/Tugas/Form and Validation/Tugas From Register.jsx"
 
 function App() {
   return (
       <div>
-        <TugasFromRegister/>
+        <Redux/>
       </div>
     );
 }
